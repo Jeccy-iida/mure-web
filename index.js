f72c00008a49d8bbf7a0f4b117e484ec853214e1
Jeccy-iida <55146750+Jeccy-iida@users.noreply.github.com>
2019-10-11 22:03:59 +0900

    top title

--- index.js
+++ index.js
@@ -44,3 +44,21 @@ $(function () {
     /*オプションを足す場合はここへ記載*/
 
 });
+
+$(function () {
+    // スクロールしたときに実行
+    $(window).scroll(function () {
+        // 目的のスクロール量を設定(px)
+        var TargetPos = 350;
+        // 現在のスクロール位置を取得
+        var ScrollPos = $(window).scrollTop();
+        // 現在位置が目的のスクロール量に達しているかどうかを判断
+        if (ScrollPos >= TargetPos) {
+            // 達していれば表示
+            $("#topbutton").fadeIn();
+        } else {
+            // 達していなければ非表示
+            $("#topbutton").fadeOut();
+        }
+    });
+});
